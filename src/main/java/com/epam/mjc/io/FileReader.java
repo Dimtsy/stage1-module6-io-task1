8471b9d2faefa936450d7ca02eac80f3fb26fbf3
Dzmitry Tsybulski <dzmitrytsybulski@gmail.com>
2023-04-05 22:45:02 +0300

    Create Stage 1 Task IO Task 1 Correct 3

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -5,20 +5,24 @@ import java.io.*;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        try {
-            FileInputStream fin = new FileInputStream(file);
+        try (FileInputStream fin = new FileInputStream(file)) {
             DataInputStream in = new DataInputStream(fin);
-            BufferedReader out = new BufferedReader(new InputStreamReader(in));
-            String strLine;
-            String[][] subStr = new String[out.read()][2];
-            int i = 0;
-            while ((strLine = out.readLine()) != null) {
-                subStr[i] = strLine.split(": ");
-                i++;
+            try (BufferedReader out = new BufferedReader(new InputStreamReader(in))) {
+                String strLine;
+                String[][] subStr = new String[out.read()][2];
+                int i = 0;
+                while ((strLine = out.readLine()) != null) {
+                    subStr[i] = strLine.split(": ");
+                    i++;
+                }
+                in.close();
+                out.close();
+                return new Profile(subStr[0][1], Integer.parseInt(subStr[1][1]),
+                        subStr[2][1], Long.valueOf(subStr[3][1]));
+            } catch (Exception e) {
+                e.getStackTrace();
             }
-            in.close();
-            return new Profile(subStr[0][1], Integer.parseInt(subStr[1][1]),
-                    subStr[2][1], Long.valueOf(subStr[3][1]));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -29,6 +33,5 @@ public class FileReader {
         File file = new File("src\\main\\resources\\Profile.txt");
         FileReader fileReader = new FileReader();
         fileReader.getDataFromFile(file);
-        System.out.println(fileReader.getDataFromFile(file));
     }
 }
