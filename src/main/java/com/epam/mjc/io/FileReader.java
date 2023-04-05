73f9eb2948b6bc72f07abbd5fde9ca474cea14c7
Dzmitry Tsybulski <dzmitrytsybulski@gmail.com>
2023-04-05 20:33:24 +0300

    Create Stage 1 Task IO Task 1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,45 @@
 package com.epam.mjc.io;
 
-import java.io.File;
-
+import java.io.*;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+        try (FileInputStream fin = new FileInputStream(file)) {
+            String str = "";
+            int i;
+            while ((i = fin.read()) != -1) {
+                str = str + (char) i;
+            }
+            String[] subStr;
+            String delimiter = "\r\n";
+            subStr = str.split(delimiter);
+
+            String delimiterTwo = ": ";
+
+            String[] subStrTwo = subStr[0].split(delimiterTwo);
+            String name = subStrTwo[1];
+
+            subStrTwo = subStr[1].split(delimiterTwo);
+            Integer age = Integer.parseInt(subStrTwo[1]);
+
+            subStrTwo = subStr[2].split(delimiterTwo);
+            String email = subStrTwo[1];
+
+            subStrTwo = subStr[3].split(delimiterTwo);
+            Long phone = Long.valueOf(subStrTwo[1]);
+
+            return new Profile(name, age, email, phone);
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
         return new Profile();
+    }
+
+    public static void main(String[] args) {
+        File file = new File("src\\main\\resources\\Profile.txt");
+        FileReader fileReader = new FileReader();
+        fileReader.getDataFromFile(file);
     }
 }
